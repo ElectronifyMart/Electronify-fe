17d80b52abfac194891a652bf70eda44981dce1f
rahman <rahmannurhayadi@gmail.com>
2025-01-20 07:35:59 +0700

    menambah font awesome icon

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+
+import { library} from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+
+library.add(fas,far)
+
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+
+app.component('font-awesome-icon',FontAwesomeIcon)
+
+app.mount('#app')
