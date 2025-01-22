dc554a1b393d1696195a5f2681d830adc56c1d31
Muhammad Gilang Ramadhan <foryouandmefck17@gmail.com>
2025-01-22 08:01:05 +0700

    Update: Login,Register & Verifikasi Done

--- src/main.js
+++ src/main.js
@@ -3,20 +3,20 @@ import "./assets/tailwind.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/route";
-import pinia from "./services/pinia";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { far } from "@fortawesome/free-regular-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import Vue3EasyDataTable from 'vue3-easy-data-table';
-import 'vue3-easy-data-table/dist/style.css';
+import { createPinia } from "pinia";
+import Vue3EasyDataTable from "vue3-easy-data-table";
+import "vue3-easy-data-table/dist/style.css";
 
 library.add(fas, far);
 const app = createApp(App);
+const pinia = createPinia();
 app.use(pinia);
-
-app.component('EasyDataTable', Vue3EasyDataTable);
+app.component("EasyDataTable", Vue3EasyDataTable);
 app.component("font-awesome-icon", FontAwesomeIcon);
 
 app.use(router);
